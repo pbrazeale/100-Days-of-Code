0046db679c5f844a8220f89d09b1d0707e8005ad
Philip Brazeale <pbrazeale@gmail.com>
2025-02-09 17:38:40 -0600

    100 Days of Code 001

---
+++ Day_001_Band_Name_Generator/task.py
@@ -0,0 +1,3 @@
+band_name_city = input("Welcome to the Band Name Generator.\nWhat's the name of the city you grew up in?\n")
+band_name_pet = input("What's your pet's name?\n")
+print("Your band name could be "+band_name_city+" "+band_name_pet)
