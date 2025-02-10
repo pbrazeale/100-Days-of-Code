ffe2410cc1faa12810dd107c56eeaa5293a3d028
Philip Brazeale <pbrazeale@gmail.com>
2025-02-09 19:57:39 -0600

    100 Days of Code 003.2

--- Day_003_Treasure_Island/task.py
+++ Day_003_Treasure_Island/task.py
@@ -4,20 +4,20 @@ import sys
 Art = text2art("Treasure Island", font="fraktur")
 print(Art)
 
-print("Welcome to Terasure Island.")
-first_move = input("Would you like to go Left or Right?\n").lower()
+print("Welcome to Terasure Island.\nYour mission is to find the lost treasure.")
+first_move = input("You've stumbled into the jungle and come upon a crossroad. Would you like to go 'Left' or 'Right'?\n").lower()
 
 if first_move != "left":
     print("You fell into a hole.\nGAME OVER")
     sys.exit()
 
-second_move = input("You see a pool of water. Swim or Wait?\n").lower()
+second_move = input("You see a lake up ahead.\nThere is a small island in the middle of the lake.\nWould you like to 'Swim' toward the island or 'Wait' for a boat to arrive?\n").lower()
 
 if second_move != "wait":
     print("You were attacked by a trout.\nGAME OVER")
     sys.exit()
 
-third_move = input("You find yourslef in a room standing before three doors: Red, Yellow, and Blue.\nWhich door do you open? ").lower()
+third_move = input("You find a cave on the island and inside you're standing before three doors: Red, Yellow, and Blue.\nWhich door do you open? ").lower()
 
 if third_move == "red":
     print("You were burned by fire.\nGAME OVER")
