917307103335e1a2b04cddc56ea1c245b1dff3f0
Philip Brazeale <pbrazeale@gmail.com>
2025-02-17 09:20:40 -0600

    Update 100DoC v0.17.6

--- Day_017_Quiz_Game/Quiz Game/main.py
+++ Day_017_Quiz_Game/Quiz Game/main.py
@@ -1,11 +1,11 @@
 from question_model import Question
-from data import question_data
+from data2 import question_data
 from quiz_brain import QuizBrain
 
 question_bank = []
 for q in question_data:
-    text = q["text"]
-    answer = q["answer"]
+    text = q["question"]
+    answer = q["correct_answer"]
     question = Question(text, answer)
     question_bank.append(question)
 
@@ -13,3 +13,6 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
+
+print(f"You completed the quiz!")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
