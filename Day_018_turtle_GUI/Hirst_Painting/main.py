29abbfa4e2f072280fa21fd80de8e6e7d024dd1f
Philip Brazeale <pbrazeale@gmail.com>
2025-02-18 10:29:21 -0600

    Udpate 100DoC v0.18.11

--- Day_018_turtle_GUI/Hirst_Painting/main.py
+++ Day_018_turtle_GUI/Hirst_Painting/main.py
@@ -2,22 +2,60 @@ import turtle as T
 import colorgram
 import random
 
+T.colormode(255)
+
 colors = colorgram.extract("image.jpg", 10)
 
 timmy = T.Turtle()
+timmy.pensize(10)
+timmy.speed(0)
 
-timmy.pensize(30)
+
+def color_paint_brush(i):
+    paint_color = colors[i]
+    return paint_color.rgb
+
+
+def bottom_right(name, height, width):
+    name.up()
+    name.right(90)
+    name.forward((height / 2))
+    name.right(90)
+    name.forward((width / 2))
+    name.right(180)
+    name.down()
+
+
+def draw_dots_row(name, dots, grid_width):
+    for _ in range(dots):
+        name.color(color_paint_brush(random.randint(0, 9)))
+        name.forward(1)
+        if _ < dots - 1:
+            name.up()
+            name.forward(grid_width)
+            name.down()
 
 
 def painting(name, height, width, dots):
     grid_width = width / dots - dots
     grid_height = height / dots - dots
-    for _ in range(dots + 1):
-        name.color(random.choice(colors))
-        name.forward(1)
-        name.up
-        name.forward(grid_width)
-        name.down()
+    bottom_right(name, height, width)
+    for _ in range(dots):
+        draw_dots_row(name, dots, grid_width)
+        if _ < dots - 1:
+            name.up()
+            name.left(90)
+            name.forward(grid_height)
+            name.left(90)
+            name.forward(grid_width * 9 + 10)
+            name.right(180)
+            name.down()
 
 
-painting(timmy, 200, 200, 10)
+painting(timmy, 800, 800, 10)
+
+
+# print(color_paint_brush(random.randint(0, 9)))
+
+screen = T.Screen()
+screen.exitonclick()
