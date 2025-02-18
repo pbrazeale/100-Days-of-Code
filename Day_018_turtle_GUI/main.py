35d16a15d9cd70b90dc3cb4594db423707473199
Philip Brazeale <pbrazeale@gmail.com>
2025-02-18 09:18:46 -0600

    Update 100DoC v0.18.6

--- Day_018_turtle_GUI/main.py
+++ Day_018_turtle_GUI/main.py
@@ -1,61 +1,7 @@
 from turtle import Turtle, Screen
+import random
 
-
-def draw_triangle(name, num):
-    for _ in range(3):
-        name.forward(num)
-        name.right(120)
-
-
-def draw_square(name, num):
-    for _ in range(4):
-        name.forward(num)
-        name.right(90)
-
-
-def draw_pentagon(name, num):
-    for _ in range(5):
-        name.forward(num)
-        name.right((72))
-
-
-def draw_hexagon(name, num):
-    for _ in range(6):
-        name.forward(num)
-        name.right((60))
-
-
-def draw_heptagon(name, num):
-    for _ in range(7):
-        name.forward(num)
-        name.right((51.4285))
-
-
-def draw_octagon(name, num):
-    for _ in range(8):
-        name.forward(num)
-        name.right((45))
-
-
-def draw_nonagon(name, num):
-    for _ in range(9):
-        name.forward(num)
-        name.right((40))
-
-
-def draw_decagon(name, num):
-    for _ in range(10):
-        name.forward(num)
-        name.right((36))
-
-
-def draw_dash_line(name, num):
-    line = round(num / 20)
-    for _ in range(int(num / (line * 2))):
-        name.forward(line)
-        name.penup()
-        name.forward(line)
-        name.pendown()
+colors = ["blue3", "red", "black", "green", "purple"]
 
 
 def draw_shape(name, num_sides, num_forward):
@@ -65,24 +11,26 @@ def draw_shape(name, num_sides, num_forward):
         name.right(angle)
 
 
+def draw_random_walk(name, num_forward):
+    angle = random.choice([0, 90, 180, 270])
+    name.right(angle)
+    name.forward(num_forward)
+
+
 timmy = Turtle()
 timmy.shape("turtle")
 timmy.color("blue3")
-# draw_square(name, 100)
-# draw_dash_line(timmy, 100)
-
-# draw_triangle(timmy, 100)
-# draw_square(timmy, 100)
-# draw_pentagon(timmy, 100)
-# draw_hexagon(timmy, 100)
-# draw_heptagon(timmy, 100)
-# draw_octagon(timmy, 100)
-# draw_nonagon(timmy, 100)
-# draw_decagon(timmy, 100)
+timmy.pensize(10)
+timmy.speed(0)
 
 # such a clean solution!
-for _ in range(3, 11):
-    draw_shape(timmy, _, 100)
+# for _ in range(3, 11):
+#     timmy.color(random.choice(colors))
+#     draw_shape(timmy, _, 100)
+
+for _ in range(30):
+    timmy.color(random.choice(colors))
+    draw_random_walk(timmy, 20)
 
 screen = Screen()
 screen.exitonclick()
