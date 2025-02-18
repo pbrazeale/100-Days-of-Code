fc28205b8567846abe88b77369c2f83e5c910847
Philip Brazeale <pbrazeale@gmail.com>
2025-02-18 13:30:03 -0600

    Update 100DoC v0.19.1

--- Day_019_turtle_race/main.py
+++ Day_019_turtle_race/main.py
@@ -1,1 +1,14 @@
-ÿþ
+import turtle as T
+
+timmy = T.Turtle()
+screen = T.Screen()
+
+
+def move_forward():
+    timmy.forward(10)
+
+
+screen.listen()
+screen.onkey(key="space", fun=move_forward)
+
+screen.exitonclick()
