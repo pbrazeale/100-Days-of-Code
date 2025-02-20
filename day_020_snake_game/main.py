80f4e06ce3feb3577fa7d299bf840349fa650b8c
Philip Brazeale <pbrazeale@gmail.com>
2025-02-20 09:20:24 -0600

    Update 100DoC v0.20.2

--- day_020_snake_game/main.py
+++ day_020_snake_game/main.py
@@ -1,23 +1,35 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
-# Make head
-snake_head = Turtle("square")
-snake_head.color("white")
-snake_head.penup()
-# Create first body
-snake_body = Turtle("square")
-snake_body.color("white")
-snake_body.penup()
-snake_body.setx(snake_head.pos()[0] - 20)
-# Create tail
-snake_tail = Turtle("square")
-snake_tail.color("white")
-snake_tail.penup()
-snake_tail.setx(snake_body.pos()[0] - 20)
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+segments = []
+
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.penup()
+    new_segment.color("white")
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+screen.update()
+
+game_is_one = True
+while game_is_one:
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+
+    segments[0].forward(20)
 
 screen.exitonclick()
