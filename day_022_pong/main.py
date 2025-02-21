4e20874ebd82cd1c7ddcdd58fa9d9ceef2c46804
Philip Brazeale <pbrazeale@gmail.com>
2025-02-21 09:20:23 -0600

    Udpate 100DoC v0.22.2

--- day_022_pong/main.py
+++ day_022_pong/main.py
@@ -1,8 +1,38 @@
 from turtle import Turtle, Screen
 
 screen = Screen()
-screen.screensize(400, 1000)
 screen.bgcolor("black")
+screen.title("Pong Remake")
+screen.setup(800, 600)
+screen.tracer(0)
+
+paddle = Turtle()
+# paddle.hideturtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+# paddle.showturtle()
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 screen.exitonclick()
