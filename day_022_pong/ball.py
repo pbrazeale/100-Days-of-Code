12cc0bdd8c9204f2d34a80c08ea429f87ca72fda
Philip Brazeale <pbrazeale@gmail.com>
2025-02-21 10:04:31 -0600

    v0.22.7 Create Ball move

--- day_022_pong/ball.py
+++ day_022_pong/ball.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import random
 
 
 class Ball(Turtle):
@@ -7,3 +8,12 @@ class Ball(Turtle):
         self.shape("square")
         self.color("white")
         self.penup()
+
+    # def volley(self):
+    #     self.setheading(random.randint(0, 360))
+    #     self.forward(20)
+
+    def move(self):
+        new_x = self.xcor() + 10
+        new_y = self.xcor() + 10
+        self.goto(new_x, new_y)
--- day_022_pong/main.py
+++ day_022_pong/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -21,7 +22,9 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 # test
 screen.exitonclick()
