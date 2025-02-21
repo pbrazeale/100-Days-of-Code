567257a8012c151bc8127fab22536faaabd009cb
Philip Brazeale <pbrazeale@gmail.com>
2025-02-21 09:41:02 -0600

    v0.22.4 Update class Paddle

--- day_022_pong/main.py
+++ day_022_pong/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
@@ -6,24 +7,8 @@ screen.title("Pong Remake")
 screen.setup(800, 600)
 screen.tracer(0)
 
-paddle = Turtle()
-# paddle.hideturtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
-# paddle.showturtle()
-
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 
 screen.listen()
