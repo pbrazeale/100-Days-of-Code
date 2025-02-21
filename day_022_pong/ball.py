9a0503f4e3e339606559408023bc24f6d40a0fa7
Philip Brazeale <pbrazeale@gmail.com>
2025-02-21 09:51:22 -0600

    v0.22.6 Create class Ball

---
+++ day_022_pong/ball.py
@@ -0,0 +1,9 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
