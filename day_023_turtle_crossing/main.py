964ecc5c55f16332d8dfa7bb12d8d6c7aa39f0ab
Philip Brazeale <pbrazeale@gmail.com>
2025-02-22 07:17:13 -0600

    v0.23.7 Create Player move

--- day_023_turtle_crossing/main.py
+++ day_023_turtle_crossing/main.py
@@ -8,7 +8,16 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkeypress(player.move, "Up")
+
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
