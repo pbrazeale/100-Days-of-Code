230edf74e0414e7658bad00645b00c03d93f8349
Philip Brazeale <pbrazeale@gmail.com>
2025-02-22 08:15:38 -0600

    v0.23.14 Update CarManager random_chance

--- day_023_turtle_crossing/car_manager.py
+++ day_023_turtle_crossing/car_manager.py
@@ -12,14 +12,16 @@ class CarManager:
         self.all_cars = []
 
     def create_car(self):
-        car = Turtle("square")
-        car.color(random.choice(COLORS))
-        car.penup()
-        car.left(180)
-        car.shapesize(stretch_wid=1, stretch_len=2)
-        start_y = random.randint(-250, 250)
-        car.goto(START_X, start_y)
-        self.all_cars.append(car)
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            car = Turtle("square")
+            car.color(random.choice(COLORS))
+            car.penup()
+            car.left(180)
+            car.shapesize(stretch_wid=1, stretch_len=2)
+            start_y = random.randint(-250, 250)
+            car.goto(START_X, start_y)
+            self.all_cars.append(car)
 
     def move(self):
         for car in self.all_cars:
