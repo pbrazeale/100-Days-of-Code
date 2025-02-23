dcc1ba2d3acb60088dc2c9ebcc4887065c22f18e
Philip Brazeale <pbrazeale@gmail.com>
2025-02-23 12:47:16 -0600

    v0.24.9 Update Main open -a

--- day_024_mail_merge/file_lesson/main.py
+++ day_024_mail_merge/file_lesson/main.py
@@ -2,6 +2,11 @@
 # Solution https://stackoverflow.com/questions/22282760/filenotfounderror-errno-2-no-such-file-or-directory (Windows vs Linux issue)
 # used \\ because the frist \ escapes the character.
 
-file = open("..\\100 Days of Code\\day_024_mail_merge\\file_lesson\\my_file.txt")
-contents = file.read()
-print(contents)
+# Corrected issue by chaging my terminal working dir. -_-
+
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+with open("new_file.txt", mode="a") as file:
+    file.write("\nNew text.")
