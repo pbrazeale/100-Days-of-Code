88d513172e929da317acefa6042a51663fdd30f5
Philip Brazeale <pbrazeale@gmail.com>
2025-02-23 12:16:00 -0600

    v0.24.2 Update Scoreboard reset

--- day_024_mail_merge/snake_game_update/scoreboard.py
+++ day_024_mail_merge/snake_game_update/scoreboard.py
@@ -12,16 +12,23 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 250)
         self.score_num = 0
+        self.high_score = 0
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        self.clear()
         self.write(f"Score: {self.score_num}", align=ALIGNMENT, font=FONT)
 
     def score(self):
         self.score_num += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score_num > self.high_score:
+            self.high_score = self.score_num
+        self.score_num = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
