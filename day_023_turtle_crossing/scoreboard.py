febf11a9010ad943670cb1c42fc2a152c7dd89fb
Philip Brazeale <pbrazeale@gmail.com>
2025-02-23 11:06:46 -0600

    v0.23.21 Update Scoreboard update_scoreboard

--- day_023_turtle_crossing/scoreboard.py
+++ day_023_turtle_crossing/scoreboard.py
@@ -10,7 +10,12 @@ class Scoreboard(Turtle):
         self.penup()
         self.level = 1
         self.goto(-280, 260)
+        self.udpate_scoreboard
+
+    def udpate_scoreboard(self):
+        self.clear()
         self.write(f"Level: {self.level}", align="left", font=FONT)
 
     def level_up(self):
         self.level += 1
+        self.udpate_scoreboard
