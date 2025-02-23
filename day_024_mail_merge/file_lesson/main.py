631dc6e225e23b7a7c4add9b7756d816fb101c9b
Philip Brazeale <pbrazeale@gmail.com>
2025-02-23 12:42:43 -0600

    v0.24.8 Update Main open 'my_file.txt'

--- day_024_mail_merge/file_lesson/main.py
+++ day_024_mail_merge/file_lesson/main.py
@@ -1,5 +1,7 @@
 # Gave error, reading https://docs.python.org/3/library/functions.html#open
+# Solution https://stackoverflow.com/questions/22282760/filenotfounderror-errno-2-no-such-file-or-directory (Windows vs Linux issue)
+# used \\ because the frist \ escapes the character.
 
-file = open("my_file.txt")
+file = open("..\\100 Days of Code\\day_024_mail_merge\\file_lesson\\my_file.txt")
 contents = file.read()
 print(contents)
