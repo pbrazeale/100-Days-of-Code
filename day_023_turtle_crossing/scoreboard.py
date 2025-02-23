a4e95ef4f534b65e4a2e5e48f189696080c80968
Philip Brazeale <pbrazeale@gmail.com>
2025-02-23 11:03:48 -0600

    v0.23.19 Update Scoreboard __init__

--- day_023_turtle_crossing/scoreboard.py
+++ day_023_turtle_crossing/scoreboard.py
@@ -1,5 +1,13 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.level = 1
+        self.goto(-280, 260)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
