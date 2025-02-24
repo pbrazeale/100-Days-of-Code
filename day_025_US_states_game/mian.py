50121c1f50d962a9bc1fdfcbd3851ab09915550b
Philip Brazeale <pbrazeale@gmail.com>
2025-02-24 11:56:42 -0600

    v0.25.4 Import pandas and refactor

--- day_025_US_states_game/mian.py
+++ day_025_US_states_game/mian.py
@@ -1,12 +1,19 @@
-import csv
+# import csv
+
+# # with open("weather_data.csv") as weather_data:
+# #     data = weather_data.readlines()
 
 # with open("weather_data.csv") as weather_data:
-#     data = weather_data.readlines()
+#     data = csv.reader(weather_data)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#         print(temperatures)
 
-with open("weather_data.csv") as weather_data:
-    data = csv.reader(weather_data)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-        print(temperatures)
+# Pandas doc https://pandas.pydata.org/docs/
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+
+print(data["temp"])
