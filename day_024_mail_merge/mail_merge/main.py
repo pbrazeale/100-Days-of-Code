54949ea29b4bd3fbd39e7b97bdcba17f371ea493
Philip Brazeale <pbrazeale@gmail.com>
2025-02-24 09:50:50 -0600

    v0.24.20 Update Main open functions realative file paths

--- day_024_mail_merge/mail_merge/main.py
+++ day_024_mail_merge/mail_merge/main.py
@@ -1,18 +1,21 @@
 # TODO: Create a letter using starting_letter.txt
-with open("./Input/Letters/starting_letter.txt", "r") as letter:
-    pass
 # for each name in invited_names.txt
-with open("./Input/Names/invited_names.txt", "r") as invite:
-    names = invite.readlines()
+with open("day_024_mail_merge\\mail_merge\\Input\\Names\\invited_names.txt") as invites:
+    names = invites.readlines()
 # Replace the [name] placeholder with the actual name.
-with open("./Input/Letters/starting_letter.txt", "w") as letter:
+with open(
+    "day_024_mail_merge\\mail_merge\\Input\\Letters\\starting_letter.txt"
+) as letter:
     letter_contents = letter.read()
     for name in names:
-        new_letter = letter_contents.replace("[name]", name)
-
-# Save the letters in the folder "ReadyToSend".
-with open("./Output/ReadyToSend/", "w") as letter:
-    pass
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace("[name]", stripped_name)
+        # Save the letters in the folder "ReadyToSend".
+        with open(
+            f"day_024_mail_merge\\mail_merge\\Output\\ReadyToSend\\letter_for_{stripped_name}.txt",
+            "w",
+        ) as formatted_letter:
+            formatted_letter.write(new_letter)
 
 # Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
