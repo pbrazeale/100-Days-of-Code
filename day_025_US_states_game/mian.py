c3afeabed294d63effc5b642d12cb24d5c04b107
Philip Brazeale <pbrazeale@gmail.com>
2025-02-25 08:45:01 -0600

    v0.25.7 Update Main squirrel_count first attempt

--- day_025_US_states_game/mian.py
+++ day_025_US_states_game/mian.py
@@ -1,45 +1,70 @@
-# import csv
+# # import csv
+
+# # # with open("weather_data.csv") as weather_data:
+# # #     data = weather_data.readlines()
 
 # # with open("weather_data.csv") as weather_data:
-# #     data = weather_data.readlines()
+# #     data = csv.reader(weather_data)
+# #     temperatures = []
+# #     for row in data:
+# #         if row[1] != "temp":
+# #             temperatures.append(int(row[1]))
+# #         print(temperatures)
 
-# with open("weather_data.csv") as weather_data:
-#     data = csv.reader(weather_data)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != "temp":
-#             temperatures.append(int(row[1]))
-#         print(temperatures)
+# # Pandas doc https://pandas.pydata.org/docs/
+# import pandas
 
-# Pandas doc https://pandas.pydata.org/docs/
+# # data = pandas.read_csv("weather_data.csv")
+
+# # print(type(data))
+
+# # print(data["temp"])
+
+# # data_dict = data.to_dict()
+# # print(data_dict)
+
+# # temp_list = data["temp"].to_list()
+# # print(temp_list)
+
+# # avg_temp = data["temp"].mean()
+# # max_temp = data.temp.max()
+
+# # print(max_temp)
+
+# # print(data[data.day == "Monday"])
+# # print(data[data.temp == max_temp])
+
+# # monday = data[data.day == "Monday"]
+# # temp_to_f = monday.temp[0] * (9 / 5) + 32
+# # print(temp_to_f)
+
+# # Create a dataframe from scratch
+# data_dict = {"students": ["Amy", "James", "Angela"], "scores": [76, 56, 65]}
+# data = pandas.DataFrame(data_dict)
+# print(data)
+# data.to_csv("new_data.csv")
+
+import csv
 import pandas
 
-# data = pandas.read_csv("weather_data.csv")
+# idneitfy number of Gray, Cinnamon, Black squirrels
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census.csv")
+primary_colors = data["Primary Fur Color"]
+# gray
+gray = primary_colors == "Gray"
+gray_count = gray.count()
+# print(gray_count)
+cinnamon = primary_colors == "Cinnamon"
+cinnamon_count = cinnamon.count()
 
-# print(type(data))
+black = primary_colors == "Black"
+black_count = black.count()
 
-# print(data["temp"])
-
-# data_dict = data.to_dict()
-# print(data_dict)
-
-# temp_list = data["temp"].to_list()
-# print(temp_list)
-
-# avg_temp = data["temp"].mean()
-# max_temp = data.temp.max()
-
-# print(max_temp)
-
-# print(data[data.day == "Monday"])
-# print(data[data.temp == max_temp])
-
-# monday = data[data.day == "Monday"]
-# temp_to_f = monday.temp[0] * (9 / 5) + 32
-# print(temp_to_f)
-
-# Create a dataframe from scratch
-data_dict = {"students": ["Amy", "James", "Angela"], "scores": [76, 56, 65]}
-data = pandas.DataFrame(data_dict)
-print(data)
-data.to_csv("new_data.csv")
+# Build new CSV
+# Fur Color, Count
+processed_primary_colors = {
+    "Fur Color": ["gray", "cinnamon", "balck"],
+    "Count": [gray_count, cinnamon_count, black_count],
+}
+squirrel_count = pandas.DataFrame(processed_primary_colors)
+print(squirrel_count)
