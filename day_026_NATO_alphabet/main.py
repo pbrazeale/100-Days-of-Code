557f3890bd0207b677c5b02f1d03820338af6fee
Philip Brazeale <pbrazeale@gmail.com>
2025-02-26 08:01:14 -0600

    v0.26.2 Update Main list_of_strings

--- day_026_NATO_alphabet/main.py
+++ day_026_NATO_alphabet/main.py
@@ -5,8 +5,13 @@ numbers = [1, 2, 3]
 #     new_list.append(add_1)
 
 # List comperhension
-# new_list = [new_item for item in list]
+# new_list = [new_item for item in list `if test`]
 new_list = [n + 1 for n in numbers]
 
 double_list = [n * 2 for n in range(1, 5)]
-print(double_list)
+
+
+list_of_strings = ["9", "0", "32", "8", "2", "8", "64", "29", "42", "99"]
+numbers = [int(n) for n in list_of_strings]
+result = [n for n in numbers if n % 2 == 0]
+print(result)
