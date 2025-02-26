b3677508c05aabcf9bb9a74b8ef13f5c184f7378
Philip Brazeale <pbrazeale@gmail.com>
2025-02-26 14:49:42 -0600

    v0.27.2 Update Label

--- day_027_tkinter/main.py
+++ day_027_tkinter/main.py
@@ -1,5 +1,12 @@
 import tkinter
 
 window = tkinter.Tk()
+window.title("My first GUI")
+window.minsize(width=500, height=300)
+
+# Label
+my_lable = tkinter.Label(text="I am a label")
+my_lable.pack()
+
 
 window.mainloop()
