6198edb313e168bc86cf7fd31a7f8c786d06f86b
Philip Brazeale <pbrazeale@gmail.com>
2025-02-28 09:05:40 -0600

    v.0.27.8 Create mile_to_km

--- day_027_tkinter/main.py
+++ day_027_tkinter/main.py
@@ -4,10 +4,13 @@ from tkinter import *
 window = Tk()
 window.title("My first GUI")
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=100)
 
 # Label
 my_lable = Label(text="I am a label", font=("Arial", 24, "bold"))
-my_lable.pack()
+# my_lable.place(x=0, y=0)
+my_lable.grid(column=0, row=0)
+my_lable.config(padx=20, pady=20)
 my_lable.config(text="New Text")
 
 
@@ -18,10 +21,13 @@ def button_clicked():
 
 
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+
+new_button = Button(text="new button", command=button_clicked)
+new_button.grid(column=2, row=0)
 
 # Entry
 input = Entry(width=10)
-input.pack()
+input.grid(column=3, row=2)
 
 window.mainloop()
---
+++ day_027_tkinter/mile_to_km.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+window = Tk()
+window.title("Mile to Km Converter")
+window.minsize(width=200, height=100)
+window.config(padx=50, pady=50)
+
+miles_input = Entry(width=10)
+miles_input.grid(column=1, row=0)
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+equal_label = Label(text="is equal to")
+equal_label.grid(column=0, row=1)
+
+km_int_label = Label(text="0")
+km_int_label.grid(column=1, row=1)
+
+km_label = Label(text="Km")
+km_label.grid(column=2, row=1)
+
+
+def calculate():
+    km_int = f"{int(miles_input.get()) * 1.6: .2f}"
+    km_int_label.config(text=km_int)
+
+
+calc_btn = Button(text="Calculate", command=calculate)
+calc_btn.grid(column=1, row=2)
+
+window.mainloop()
