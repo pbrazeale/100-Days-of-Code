7f43bb1de2944ca114997143f2e0ffa2e9fbb0c5
Philip Brazeale <pbrazeale@gmail.com>
2025-02-28 08:33:48 -0600

    v.0.27.7 Import other_tkinter_widgets

--- day_027_tkinter/main.py
+++ day_027_tkinter/main.py
@@ -1,12 +1,27 @@
-import tkinter
+# import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI")
 window.minsize(width=500, height=300)
 
 # Label
-my_lable = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_lable.pack(side="bottom")
+my_lable = Label(text="I am a label", font=("Arial", 24, "bold"))
+my_lable.pack()
+my_lable.config(text="New Text")
 
+
+# Button
+def button_clicked():
+    label_title = input.get()
+    my_lable.config(text=label_title)
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+input = Entry(width=10)
+input.pack()
 
 window.mainloop()
