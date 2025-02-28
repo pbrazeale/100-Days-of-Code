a243832337de98994c85bc3cc04e849964100f13
Philip Brazeale <pbrazeale@gmail.com>
2025-02-28 08:04:41 -0600

    v.0.27.6 Update Car

--- day_027_tkinter/playground.py
+++ day_027_tkinter/playground.py
@@ -12,3 +12,14 @@ def calculate(n, **kwargs):
 
 
 calculate(2, add=3, multiply=5)
+
+
+class Car:
+    def __init__(self, **kw):
+        # .get fills the property with None if not provided at initialization
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+
+
+my_car = Car(make="Nissan")
+print(my_car.model)
