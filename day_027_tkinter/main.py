6b74ad804f56d0d338ddc43ff473bbaae6075fb7
Philip Brazeale <pbrazeale@gmail.com>
2025-02-28 07:56:10 -0600

    v.0.27.4 Create Playground

--- day_027_tkinter/main.py
+++ day_027_tkinter/main.py
@@ -5,8 +5,8 @@ window.title("My first GUI")
 window.minsize(width=500, height=300)
 
 # Label
-my_lable = tkinter.Label(text="I am a label")
-my_lable.pack()
+my_lable = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_lable.pack(side="bottom")
 
 
 window.mainloop()
---
+++ day_027_tkinter/playground.py
@@ -0,0 +1,5 @@
+def add(*args):
+    print(sum(args))
+
+
+add(3, 6, 8)
