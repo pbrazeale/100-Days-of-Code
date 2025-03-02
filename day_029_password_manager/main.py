f7603e0589ef06a784505b263d71668a9c3d7301
Philip Brazeale <pbrazeale@gmail.com>
2025-03-02 15:09:28 -0600

    v0.29.3 Update Main GUI layout

--- day_029_password_manager/main.py
+++ day_029_password_manager/main.py
@@ -13,25 +13,30 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
-canvas = Canvas(width=400, height=400)
+canvas = Canvas(width=200, height=200)
+
+# Logo Row
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(200, 100, image=logo_img)
+canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
+# Website Row
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
 website_input = Entry(width=35)
 website_input.grid(column=1, row=1, columnspan=2)
 
+# Email Row
 email_username_label = Label(text="Email/Username:")
 email_username_label.grid(column=0, row=2)
 
 email_input = Entry(width=35)
 email_input.grid(column=1, row=2, columnspan=2)
 
+# Password Row
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
@@ -41,6 +46,7 @@ password_input.grid(column=1, row=3)
 password_btn = Button(text="Generate Password", command=generate_password)
 password_btn.grid(column=2, row=3)
 
+# Add Password Row
 add_password_btn = Button(text="Add", width=36, command=add_password())
 add_password_btn.grid(column=1, row=4, columnspan=2)
 
