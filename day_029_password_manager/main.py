9f18cc179880e162a391ae1a2acf271374386015
Philip Brazeale <pbrazeale@gmail.com>
2025-03-02 16:10:27 -0600

    v0.29.11 Update Main generate_password

--- day_029_password_manager/main.py
+++ day_029_password_manager/main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
-import random
+from random import choice, randint, shuffle
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -62,28 +62,15 @@ def generate_password():
     numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
 
-    nr_letters = random.randint(8, 10)
-    nr_symbols = random.randint(2, 4)
-    nr_numbers = random.randint(2, 4)
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_list = password_letters + password_symbols + password_numbers
 
-    password_list = []
+    shuffle(password_list)
+    password = "".join(password_list)
 
-    for char in range(nr_letters):
-        password_list.append(random.choice(letters))
-
-    for char in range(nr_symbols):
-        password_list += random.choice(symbols)
-
-    for char in range(nr_numbers):
-        password_list += random.choice(numbers)
-
-    random.shuffle(password_list)
-
-    password = ""
-    for char in password_list:
-        password += char
-
-    print(f"Your password is: {password}")
+    password_input.insert(f"{password}")
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
