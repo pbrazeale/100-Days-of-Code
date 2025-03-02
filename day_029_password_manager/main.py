affb16c1b2f1fcc4f9c8560bf218a60014744471
Philip Brazeale <pbrazeale@gmail.com>
2025-03-02 14:22:22 -0600

    v0.29.1 Update Main logo_img

--- day_029_password_manager/main.py
+++ day_029_password_manager/main.py
@@ -3,3 +3,16 @@
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+from tkinter import *
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=600, height=600)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(300, 120, image=logo_img)
+canvas.grid(column=1, row=0)
+
+
+window.mainloop()
