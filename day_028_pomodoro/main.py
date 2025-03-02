bdbfb55df1e187e9bc0666e9a678fe3f04482c55
Philip Brazeale <pbrazeale@gmail.com>
2025-03-02 10:37:59 -0600

    v0.28.4 Update Main UI Layout

--- day_028_pomodoro/main.py
+++ day_028_pomodoro/main.py
@@ -21,10 +21,23 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bold"))
+timer_label.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_btn = Button(text="Start", highlightthickness=0)
+start_btn.grid(column=0, row=2)
+
+reset_btn = Button(text="Reset", highlightthickness=0)
+reset_btn.grid(column=2, row=2)
+
+check_mark_label = Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"))
+check_mark_label.grid(column=1, row=3)
+
 
 window.mainloop()
