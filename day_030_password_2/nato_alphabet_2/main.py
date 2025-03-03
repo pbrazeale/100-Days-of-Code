658550767e6a52af0437df1a95d8dfebb2d3c6c6
Philip Brazeale <pbrazeale@gmail.com>
2025-03-03 10:40:49 -0600

    v0.30.10 Refactor nato_alphabet_2

--- day_030_password_2/nato_alphabet_2/main.py
+++ day_030_password_2/nato_alphabet_2/main.py
@@ -12,6 +12,10 @@ phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 def get_word():
     word = input("Enter a word: ").upper()
+    convert_word(word)
+
+
+def convert_word(word):
     try:
         output_list = [phonetic_dict[letter] for letter in word]
         print(output_list)
