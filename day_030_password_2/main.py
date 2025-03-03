a2805ba94d675507fe053db9f6c7a9b0311fbc12
Philip Brazeale <pbrazeale@gmail.com>
2025-03-03 10:17:17 -0600

    v0.30.3 Update Main except KeyError

--- day_030_password_2/main.py
+++ day_030_password_2/main.py
@@ -6,6 +6,12 @@
 
 try:
     file = open("a_file.txt")
+    a_dictonary = {"key": "value"}
+    print(a_dictonary["not_a_valid_key"])
+
 # best practice to catch the exact errors you want to handle, rather than all.
 except FileNotFoundError:
     file = open("a_file.txt", "w")
+
+except KeyError as error_message:
+    print(f"The Key {error_message} does not exisit.")
