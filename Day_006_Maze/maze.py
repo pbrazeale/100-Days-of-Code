526875cae9e69cfea6d1d77fe67e73f0f40c34f9
Philip Brazeale <pbrazeale@gmail.com>
2025-03-03 10:12:50 -0600

    v0.30.0 Create Main exceptions

--- Day_006_Maze/maze.py
+++ Day_006_Maze/maze.py
@@ -1,19 +1,23 @@
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 
+
 def turn_around():
     turn_left()
     turn_left()
+
+
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
 
-#move to wall to start crawling right wall, prevents square void edge case.
+
+# move to wall to start crawling right wall, prevents square void edge case.
 while front_is_clear():
     move()
-turn_left()
+    turn_left()
 
-    
+
 while not at_goal():
     if right_is_clear():
         turn_right()
@@ -22,4 +26,3 @@ while not at_goal():
         move()
     else:
         turn_left()
-                
---
+++ day_030_password_2/main.py
@@ -0,0 +1,10 @@
+# Exceptions
+# try: something to do
+# except: do when expection arises
+# else: do this if there were no expections
+# finally: No matter what run
+
+try:
+    file = open("a_file.txt")
+except:
+    print("there was an error")
