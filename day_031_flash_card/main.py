b5e9713851514be2ee5f29094ba70796ebff9215
Philip Brazeale <pbrazeale@gmail.com>
2025-03-03 14:33:45 -0600

    v0.31.2 Update canvas

--- day_031_flash_card/main.py
+++ day_031_flash_card/main.py
@@ -1,2 +1,22 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+
+window = Tk()
+window.title("flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+# import images
+right_image = PhotoImage(file="./images/right.png")
+wrong_image = PhotoImage(file="./images/wrong.png")
+card_front_image = PhotoImage(file="./images/card_front.png")
+card_back_image = PhotoImage(file="./images/card_back.png")
+
+# Create Canvas
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.create_image(410, 273, image=card_front_image)
+canvas.grid(column=0, row=0, columnspan=2)
+
+
+window.mainloop()
