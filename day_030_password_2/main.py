0d9f57ee0b94560f5c20b68f1c93615d83ef83d9
Philip Brazeale <pbrazeale@gmail.com>
2025-03-03 11:25:10 -0600

    v0.30.14 Update Main try cycle

--- day_030_password_2/main.py
+++ day_030_password_2/main.py
@@ -96,15 +96,22 @@ def save():
             title="Oops", message="Please make sure you haven't left any fields empty."
         )
     else:
-        with open("data.json", "r") as data_file:
-            # read/load old data
-            data = json.load(data_file)
+        try:
+            with open("data.json", "r") as data_file:
+                # read/load old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
             # udpate data with new_data
             data.update(new_data)
-        with open("data.json", "w") as data_file:
-            # write updated data to data.json
-            json.dump(data, data_file, indent=4)
 
+            with open("data.json", "w") as data_file:
+                # write updated data to data.json
+                json.dump(data, data_file, indent=4)
+
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
