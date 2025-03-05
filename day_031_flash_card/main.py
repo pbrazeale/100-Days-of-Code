f1d882cc3190873e90df18c90cab95b8d6011b8b
Philip Brazeale <pbrazeale@gmail.com>
2025-03-05 10:57:52 -0600

    v0.31.10 Update Main flip_timer

--- day_031_flash_card/main.py
+++ day_031_flash_card/main.py
@@ -10,16 +10,19 @@ current_card = {}
 
 
 def next_card():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn_dict)
     current_card["French"]
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="balck")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="balck")
+    canvas.itemconfig(card_background, image=card_front_image)
+    flip_timer = window.after(3000, func=flip_card)
 
 
 def flip_card():
-    canvas.itemconfig(card_title, text="English")
-    canvas.itemconfig(card_word, text=current_card["English"])
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_image)
 
 
@@ -27,7 +30,7 @@ window = Tk()
 window.title("flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 # import images
 right_image = PhotoImage(file="./images/right.png")
