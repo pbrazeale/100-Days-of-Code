28a684414f7cd1b895f4066d035fc9668d24aa5b
Philip Brazeale <pbrazeale@gmail.com>
2025-03-07 09:43:47 -0600

    v0.32.7 Update quote_email check_date

--- day_032_automate_birthday/quote_email.py
+++ day_032_automate_birthday/quote_email.py
@@ -29,3 +29,14 @@ def send_email():
             to_addrs=target_email,
             msg=(f"Subject:{email_subject}\n\n{email_body}"),
         )
+
+
+def check_date():
+    global now, quote_of_the_day
+    if now.weekday() == 4:
+        select_quote()
+        print(quote_of_the_day)
+        # send_email()
+
+
+check_date()
