4010f6ae34f8771ea86841d096670d509a0eabdc
Philip Brazeale <pbrazeale@gmail.com>
2025-03-07 09:53:31 -0600

    v0.32.10 Update main now

--- day_032_automate_birthday/automate_birthday/main.py
+++ day_032_automate_birthday/automate_birthday/main.py
@@ -1,13 +1,15 @@
 ##################### Extra Hard Starting Project ######################
+import datetime as dt
 
 # 1. Update the birthdays.csv
 
 # 2. Check if today matches a birthday in the birthdays.csv
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day = now.day
+
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
 # 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
