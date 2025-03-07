2ba86434f38c50afd02a06498baf5fe86f60d77f
Philip Brazeale <pbrazeale@gmail.com>
2025-03-07 10:06:37 -0600

    v0.32.12 Update main data

--- day_032_automate_birthday/automate_birthday/main.py
+++ day_032_automate_birthday/automate_birthday/main.py
@@ -8,15 +8,13 @@ from vars import *
 letter = ""
 birthday_email = ""
 
-# 1. Update the birthdays.csv
+data = pandas.read_csv("birthdays.csv")
 
 # 2. Check if today matches a birthday in the birthdays.csv
 now = dt.datetime.now()
-year = now.year
 month = now.month
 day = now.day
-
-now_format = f"{year},{month},{day}"
+now_format = (month, day)
 
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
