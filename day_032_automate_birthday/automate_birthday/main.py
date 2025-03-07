3df34a135f4a5bbb341c7be28d03c95cc730fd43
Philip Brazeale <pbrazeale@gmail.com>
2025-03-07 09:59:25 -0600

    v0.32.11 Update main send_email

--- day_032_automate_birthday/automate_birthday/main.py
+++ day_032_automate_birthday/automate_birthday/main.py
@@ -1,5 +1,12 @@
 ##################### Extra Hard Starting Project ######################
 import datetime as dt
+import smtplib
+import pandas
+import random
+from vars import *
+
+letter = ""
+birthday_email = ""
 
 # 1. Update the birthdays.csv
 
@@ -9,7 +16,23 @@ year = now.year
 month = now.month
 day = now.day
 
+now_format = f"{year},{month},{day}"
+
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
+
 # 4. Send the letter generated in step 3 to that person's email address.
+def send_email():
+    global letter, birthday_email
+    email_subject = "Happy Birthday"
+    email_body = letter
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=test_email, password=test_email_password)
+        connection.sendmail(
+            from_addr=test_email,
+            to_addrs=birthday_email,
+            msg=(f"Subject:{email_subject}\n\n{email_body}"),
+        )
