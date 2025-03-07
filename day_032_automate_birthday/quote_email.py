e256548a89a9437f8b0c102ef52d7f00a824c732
Philip Brazeale <pbrazeale@gmail.com>
2025-03-07 09:43:27 -0600

    v0.32.6 Update quote_email send_email

--- day_032_automate_birthday/quote_email.py
+++ day_032_automate_birthday/quote_email.py
@@ -14,3 +14,18 @@ def select_quote():
         quotes = file.readlines()
 
     quote_of_the_day = random.choice(quotes).strip()
+
+
+def send_email():
+    global quote_of_the_day
+    email_subject = "Quote Of The Day"
+    email_body = quote_of_the_day
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=test_email, password=test_email_password)
+        connection.sendmail(
+            from_addr=test_email,
+            to_addrs=target_email,
+            msg=(f"Subject:{email_subject}\n\n{email_body}"),
+        )
