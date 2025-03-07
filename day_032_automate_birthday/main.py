01717ab614ab4fc02a27d9ca17c002312d1933f8
Philip Brazeale <pbrazeale@gmail.com>
2025-03-07 09:18:06 -0600

    v0.32.3 Update main email_subject and email_body

--- day_032_automate_birthday/main.py
+++ day_032_automate_birthday/main.py
@@ -2,8 +2,15 @@ import smtplib
 from vars import *
 
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=test_email, password=test_email_password)
-connection.sendmail(from_addr=test_email, to_addrs=target_email, msg="Hello.")
-connection.close()
+email_subject = "Hello"
+email_body = "This is the body of the email."
+
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=test_email, password=test_email_password)
+    connection.sendmail(
+        from_addr=test_email,
+        to_addrs=target_email,
+        msg=(f"Subject:{email_subject}\n\n{email_body}"),
+    )
