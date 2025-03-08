cf1260404ab85733250f94eace6ec561905948d9
Philip Brazeale <pbrazeale@gmail.com>
2025-03-08 15:18:11 -0600

    v0.33.1 Update Main rasie_for_status

--- day_033_iss_overhead_notifier/main.py
+++ day_033_iss_overhead_notifier/main.py
@@ -1,4 +1,9 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+
+# if response.status_code != 200:
+#     raise Exception(f"Error happened: {response.status_code}")
+
+# requests will handle this for us.
+response.raise_for_status()
