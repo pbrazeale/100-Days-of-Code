8b6bbfb98cbfe6707914072fa49678a500fa4775
Philip Brazeale <pbrazeale@gmail.com>
2025-03-08 15:51:32 -0600

    v0.33.8 Update ISS Main sunrise_response

--- day_033_iss_overhead_notifier/main.py
+++ day_033_iss_overhead_notifier/main.py
@@ -1,14 +1,17 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 30.267153
+MY_LNG = -97.743057
 
+iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
 # if response.status_code != 200:
 #     raise Exception(f"Error happened: {response.status_code}")
 
 # requests will handle this for us.
-response.raise_for_status()
+iss_response.raise_for_status()
 
-data = response.json()
+data = iss_response.json()
 # print(data["iss_position"])
 
 latitude = data["iss_position"]["latitude"]
@@ -16,4 +19,21 @@ longitude = data["iss_position"]["longitude"]
 
 iss_position = (latitude, longitude)
 
-print(iss_position)
+# print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0,
+}
+
+sunrise_resposne = requests.get(
+    url="https://api.sunrise-sunset.org/json", params=parameters
+)
+sunrise_resposne.raise_for_status()
+
+print(sunrise_resposne.json())
+data = sunrise_resposne.json()
+
+sunset = data["results"]["sunset"]
+sunrise = data["results"]["sunrise"]
