7d41bc1ae78817a9e1acd5c66cdeda41b7d717f1
Philip Brazeale <pbrazeale@gmail.com>
2025-03-08 16:56:23 -0600

    v0.33.13 Refactor Main iss_call and sun_call

--- day_033_iss_overhead_notifier/main.py
+++ day_033_iss_overhead_notifier/main.py
@@ -2,31 +2,33 @@ import requests
 from datetime import datetime
 import smtplib
 
+# import iss_call
+
 MY_LAT = 30.267153
 MY_LNG = -97.743057
-
-iss_latitude = float(0)
-iss_longitude = float(0)
+iss_lat = 0
+iss_lng = 0
+sunset = 0
+sunrise = 0
 
 
 def iss_call():
-    global iss_latitude, iss_longitude
-
+    global iss_lng, iss_lat
     iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
     iss_response.raise_for_status()
     data = iss_response.json()
 
-    iss_latitude = float(data["iss_position"]["latitude"])
-    iss_longitude = float(data["iss_position"]["longitude"])
+    iss_lat = float(data["iss_position"]["latitude"])
+    iss_lng = float(data["iss_position"]["longitude"])
 
 
 def sun_call():
+    global sunset, sunrise
     parameters = {
         "lat": MY_LAT,
         "lng": MY_LNG,
         "formatted": 0,
     }
-
     sunrise_resposne = requests.get(
         url="https://api.sunrise-sunset.org/json", params=parameters
     )
@@ -34,36 +36,30 @@ def sun_call():
 
     # print(sunrise_resposne.json())
     data = sunrise_resposne.json()
-
     sunset = data["results"]["sunset"]
     sunrise = data["results"]["sunrise"]
+    # print(f"sunrise {sunrise.split("T")[1].split(":")[0]}")
 
 
 def main():
-
-    # print(f"sunrise {sunrise.split("T")[1].split(":")[0]}")
-
     time_now = datetime.now()
     # print(time_now.hour)
+    sun_call()
 
-    # Check if within sight
-    within_sight = False
-    if (
-        (MY_LAT + 5) > iss_latitude
-        and (MY_LAT - 5) < iss_latitude
-        and (MY_LNG + 5) > iss_longitude
-        and (MY_LNG - 5) < iss_longitude
-    ):
-        within_sight = True
-    # print(iss_position)
-
-    # if dark and iss overhead
+    # if dark
     if (
         int(sunset.split("T")[1].split(":")[0]) - 6 < time_now.hour
         or int(sunrise.split("T")[1].split(":")[0]) - 6 > time_now.hour
-        and within_sight
     ):
-        send_email()
+        # Check if within sight
+        if (
+            (MY_LAT + 5) > iss_lat
+            and (MY_LAT - 5) < iss_lat
+            and (MY_LNG + 5) > iss_lng
+            and (MY_LNG - 5) < iss_lng
+        ):
+            # send email
+            send_email()
 
 
 def send_email():
