ba65d9ebea1ee94d3b1b5cc62434a261275262de
Philip Brazeale <pbrazeale@gmail.com>
2025-03-08 16:40:33 -0600

    v0.33.11 Update Main is dark

--- day_033_iss_overhead_notifier/main.py
+++ day_033_iss_overhead_notifier/main.py
@@ -1,51 +1,72 @@
 import requests
 from datetime import datetime
+import smtplib
 
 MY_LAT = 30.267153
 MY_LNG = -97.743057
 
-iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# if response.status_code != 200:
-#     raise Exception(f"Error happened: {response.status_code}")
+iss_latitude = float(0)
+iss_longitude = float(0)
 
-# requests will handle this for us.
-iss_response.raise_for_status()
 
-data = iss_response.json()
-# print(data["iss_position"])
+def iss_call():
+    global iss_latitude, iss_longitude
 
-latitude = float(data["iss_position"]["latitude"])
-longitude = float(data["iss_position"]["longitude"])
+    iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    iss_response.raise_for_status()
+    data = iss_response.json()
 
-iss_position = (latitude, longitude)
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-# print(iss_position)
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LNG,
-    "formatted": 0,
-}
+def main():
 
-sunrise_resposne = requests.get(
-    url="https://api.sunrise-sunset.org/json", params=parameters
-)
-sunrise_resposne.raise_for_status()
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LNG,
+        "formatted": 0,
+    }
 
-# print(sunrise_resposne.json())
-data = sunrise_resposne.json()
+    sunrise_resposne = requests.get(
+        url="https://api.sunrise-sunset.org/json", params=parameters
+    )
+    sunrise_resposne.raise_for_status()
 
-sunset = data["results"]["sunset"]
-sunrise = data["results"]["sunrise"]
+    # print(sunrise_resposne.json())
+    data = sunrise_resposne.json()
 
-# print(sunrise.split("T")[1].split(":")[0])
+    sunset = data["results"]["sunset"]
+    sunrise = data["results"]["sunrise"]
 
-time_now = datetime.now()
-print(time_now)
+    # print(f"sunrise {sunrise.split("T")[1].split(":")[0]}")
 
-# Check if within sight
-within_sight = False
-if ((MY_LAT + 5) > iss_position[0] and (MY_LAT - 5) < iss_position[0]) and (
-    (MY_LNG + 5) > iss_position[1] and (MY_LNG - 5) < iss_position[1]
-):
-    within_sight = True
+    time_now = datetime.now()
+    # print(time_now.hour)
+
+    # Check if within sight
+    within_sight = False
+    if (
+        (MY_LAT + 5) > iss_latitude
+        and (MY_LAT - 5) < iss_latitude
+        and (MY_LNG + 5) > iss_longitude
+        and (MY_LNG - 5) < iss_longitude
+    ):
+        within_sight = True
+    # print(iss_position)
+
+    # if dark and iss overhead
+    if (
+        int(sunset.split("T")[1].split(":")[0]) - 6 < time_now.hour
+        or int(sunrise.split("T")[1].split(":")[0]) - 6 > time_now.hour
+        and within_sight
+    ):
+        send_email()
+
+
+def send_email():
+    pass
+
+
+if __name__ == "__main__":
+    main()
