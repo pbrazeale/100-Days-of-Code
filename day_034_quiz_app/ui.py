fac2ca255ea49a2d41dd475ff28f141c32de9d79
Philip Brazeale <pbrazeale@gmail.com>
2025-03-09 09:06:52 -0500

    v0.34.5 Update ui.py images

--- day_034_quiz_app/ui.py
+++ day_034_quiz_app/ui.py
@@ -2,8 +2,13 @@ from tkinter import *
 
 THEME_COLOR = "#375362"
 
+false_image = PhotoImage(file="/images/false.png")
+true_image = PhotoImage(file="/images/true.png")
+
 
 class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
+
+        self.window.mainloop()
