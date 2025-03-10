4d11353706209a53cbabd6bd32224a7678078dc2
Philip Brazeale <pbrazeale@gmail.com>
2025-03-10 11:39:19 -0500

    v0.34.8 Update UI canvas

--- day_034_quiz_app/ui.py
+++ day_034_quiz_app/ui.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 THEME_COLOR = "#375362"
 
-false_image = PhotoImage(file="/images/false.png")
-true_image = PhotoImage(file="/images/true.png")
+# false_image = PhotoImage(file="/images/false.png")
+# true_image = PhotoImage(file="/images/true.png")
 
 
 class QuizInterface:
@@ -12,7 +12,13 @@ class QuizInterface:
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
-        self.score_label = Label(text="Score: 0", fg="white")
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150, 125, text="Question Goes Here", fill=THEME_COLOR
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2)
 
         self.window.mainloop()
