495f7d84d5e5461e1c41fc4c3e86ae4582b695de
Philip Brazeale <pbrazeale@gmail.com>
2025-03-11 08:23:24 -0500

    v0.34.16 Update UI get_next_question

--- day_034_quiz_app/ui.py
+++ day_034_quiz_app/ui.py
@@ -42,8 +42,15 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_net_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Scoree: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've finsihed the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def marked_true(self):
         is_right = self.quiz.check_answer("True")
