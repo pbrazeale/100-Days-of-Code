e2db706e4d0570b784a67ed33e2e7322e0955a61
Philip Brazeale <pbrazeale@gmail.com>
2025-03-14 06:49:58 -0500

    v0.36.5 Update diff_percent

--- day_036_stock_alert/main.py
+++ day_036_stock_alert/main.py
@@ -25,15 +25,15 @@ yesterday_stock_data = stock_data_list[0]
 yesterday_closing_price = yesterday_stock_data["4. close"]
 print(yesterday_closing_price)
 
-# TODO 2. - Get the day before yesterday's closing stock price
 two_days_ago_data = stock_data_list[1]
 two_days_ago_closing_price = two_days_ago_data["4. close"]
 print(two_days_ago_closing_price)
 
-# TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+difference = abs(float(yesterday_closing_price) - float(two_days_ago_closing_price))
+print(difference)
 
-
-# TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+diff_percent = (difference / float(yesterday_closing_price)) * 100
+print(diff_percent)
 
 # TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
 
