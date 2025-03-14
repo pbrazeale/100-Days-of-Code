3f3d01ce4a66dda4ef079a4d438accdb3e5862af
Philip Brazeale <pbrazeale@gmail.com>
2025-03-14 07:11:30 -0500

    v0.36.8 Update formatted_articles

--- day_036_stock_alert/main.py
+++ day_036_stock_alert/main.py
@@ -51,8 +51,10 @@ print(three_articles)
 
 ## STEP 3: Use twilio.com/docs/sms/quickstart/python
 # to send a separate message with each article's title and description to your phone number.
-
-# TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+formatted_articles = [
+    f"Headline: {article['title']} \nBrief: {article['description']}"
+    for article in three_articles
+]
 
 # TODO 9. - Send each article as a separate message via Twilio.
 
