803001fda0daf047ba5db65bfe77a8eac0351244
Philip Brazeale <pbrazeale@gmail.com>
2025-03-14 10:15:34 -0500

    v0.37.1 Update response_user

--- day_037_habit_tracker/main.py
+++ day_037_habit_tracker/main.py
@@ -1,1 +1,13 @@
 import requests
+import pixela_var
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+pixela_user_params = {
+    "token": pixela_var.token,
+    "username": pixela_var.user,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+response_user = requests.post(url=pixela_endpoint, json=pixela_user_params)
