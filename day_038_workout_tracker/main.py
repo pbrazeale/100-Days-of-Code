6b494071eb4247bceea6555b4b66d0f7efb98f1c
Philip Brazeale <pbrazeale@gmail.com>
2025-03-18 10:01:16 -0500

    v0.38.3 Create personal data

--- day_038_workout_tracker/main.py
+++ day_038_workout_tracker/main.py
@@ -1,7 +1,13 @@
 import nutritionix
 import requests
+import personal_data
 
 NUT_API_ID = nutritionix.APP_ID
 NUT_API_KEY = nutritionix.API_KEY
 
-NUT_ENDPOINT = "https://nutritionix.com/v2/natural/exercise"
+# Personal Data
+WEIGHT_KG = personal_data.weight_kg
+HEIGHT_CM = personal_data.height_cm
+AGE = personal_data.age
+
+NUT_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
