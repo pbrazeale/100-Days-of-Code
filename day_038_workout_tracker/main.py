6d577aa64455edb5fa5f2d963c6cd1608012119d
Philip Brazeale <pbrazeale@gmail.com>
2025-03-18 10:09:34 -0500

    v0.38.4 Update API request parameters

--- day_038_workout_tracker/main.py
+++ day_038_workout_tracker/main.py
@@ -2,6 +2,8 @@ import nutritionix
 import requests
 import personal_data
 
+# NUTRITIONIX
+NUT_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 NUT_API_ID = nutritionix.APP_ID
 NUT_API_KEY = nutritionix.API_KEY
 
@@ -10,4 +12,18 @@ WEIGHT_KG = personal_data.weight_kg
 HEIGHT_CM = personal_data.height_cm
 AGE = personal_data.age
 
-NUT_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+# Query
+nut_query = input(
+    "What 'exercise' did you do and for how 'long'? Exmaple: 'swam for 1 hour'\n:  "
+)
+
+
+# Requests
+headers = {"x-app-id": NUT_API_ID, "x-app-key": NUT_API_KEY}
+
+parameters = {
+    "query": nut_query,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE,
+}
