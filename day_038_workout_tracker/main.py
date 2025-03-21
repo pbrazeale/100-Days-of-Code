e0a8c5adb193da2822577b70eae5daa697e25491
Philip Brazeale <pbrazeale@gmail.com>
2025-03-21 14:06:47 -0500

    v0.38.6 Update sheety_results

--- day_038_workout_tracker/main.py
+++ day_038_workout_tracker/main.py
@@ -1,6 +1,7 @@
 import nutritionix
 import requests
 import personal_data
+import datetime as dt
 
 # NUTRITIONIX
 NUT_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
@@ -11,6 +12,12 @@ NUT_API_KEY = nutritionix.API_KEY
 WEIGHT_KG = personal_data.weight_kg
 HEIGHT_CM = personal_data.height_cm
 AGE = personal_data.age
+
+# Sheety
+SHEETY_ENDPOINT = "https://api.sheety.co/"
+SHEETY_USERNAME = "4dfcd642881f2f86ea467aeb6da6e18b"
+SHEETY_PROJECT = "workoutTraining"
+SHEETY_SHEET1 = "sheet1"
 
 # Query
 nut_query = input(
@@ -32,3 +39,9 @@ parameters = {
 nut_response = requests.post(NUT_ENDPOINT, json=parameters, headers=headers)
 nut_reults = nut_response.json()
 print(nut_reults)
+
+# Sheety API CALL
+sheety_response = requests.get(
+    f"{SHEETY_ENDPOINT}/{SHEETY_USERNAME}/{SHEETY_PROJECT}/{SHEETY_SHEET1}"
+)
+sheety_results = sheety_response.json()
