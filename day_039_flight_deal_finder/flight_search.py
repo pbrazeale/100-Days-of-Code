76f860dfe2778e7dd22b3fd67daeb09aacca761c
Philip Brazeale <pbrazeale@gmail.com>
2025-03-22 09:55:46 -0500

    v0.39.8 Update FlightSearch

--- day_039_flight_deal_finder/flight_search.py
+++ day_039_flight_deal_finder/flight_search.py
@@ -1,9 +1,13 @@
 import os
 import requests
-from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
+import datetime
 
 load_dotenv()
+
+IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
 
 
 class FlightSearch:
@@ -14,6 +18,8 @@ class FlightSearch:
         self._token = self._get_new_token()
 
     def get_destination_code(self, city_name):
+        headers = {"Authorizaiton": f"Bearer {self._token}"}
+
         code = "TESTING"
         return code
 
@@ -24,7 +30,11 @@ class FlightSearch:
             "client_id": self._api_key,
             "client_secret": self._api_secret,
         }
-        response = requests.post(url=TOKEN_ENDPOINT, headers=header, data=body)
+        response = requests.post(
+            url=TOKEN_ENDPOINT,
+            headers=header,
+            data=body,
+        )
 
         # New bearer token. Typically expires in 1799 seconds (30min)
         print(f"Your token is {response.json()['access_token']}")
