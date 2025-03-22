06af4d6ede64c19ef6788615dc5d25565912562d
Philip Brazeale <pbrazeale@gmail.com>
2025-03-22 09:46:40 -0500

    v0.39.7 Update FlightSearch

--- day_039_flight_deal_finder/flight_search.py
+++ day_039_flight_deal_finder/flight_search.py
@@ -1,5 +1,32 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
+    def __init__(self):
+        self._api_key = os.environ["AMADAUS_API_KEY"]
+        self._api_secret = os.environ["AMADAUS_API_SECRET"]
+        self._token = self._get_new_token()
+
     def get_destination_code(self, city_name):
         code = "TESTING"
         return code
+
+    def _get_new_token(self):
+        header = {"Content-Type": "application/x-www-form-urlencoded"}
+        body = {
+            "grant_type": "client_credentials",
+            "client_id": self._api_key,
+            "client_secret": self._api_secret,
+        }
+        response = requests.post(url=TOKEN_ENDPOINT, headers=header, data=body)
+
+        # New bearer token. Typically expires in 1799 seconds (30min)
+        print(f"Your token is {response.json()['access_token']}")
+        print(f"Your token expires in {response.json()['expires_in']} seconds")
+        return response.json()["access_token"]
