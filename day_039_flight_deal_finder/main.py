06bd5a7d81a549cf3b8e25f5ec8690d618c9a5ab
Philip Brazeale <pbrazeale@gmail.com>
2025-03-22 08:09:07 -0500

    v0.39.1 Update sheety_response

--- day_039_flight_deal_finder/main.py
+++ day_039_flight_deal_finder/main.py
@@ -1,1 +1,22 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from Vars import amadaus
+from day_038_workout_tracker import sheety
+import pprint
+import requests
+
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+# Sheety
+SHEETY_ENDPOINT = "https://api.sheety.co/"
+SHEETY_USERNAME = sheety.username
+SHEETY_PROJECT = "workoutTraining"
+SHEETY_SHEET1 = "sheet1"
+SHEETY_TOKEN = sheety.token
+
+sheety_header = {"Authorization": f"Bearer {SHEETY_TOKEN}"}
+sheety_endpoint = (
+    f"{SHEETY_ENDPOINT}/{SHEETY_USERNAME}/{SHEETY_PROJECT}/{SHEETY_SHEET1}"
+)
+
+sheety_response = requests.get(url=sheety_endpoint, headers=sheety_header)
+
+pprint(sheety_response.json())
