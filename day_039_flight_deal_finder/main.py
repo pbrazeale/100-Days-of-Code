258a03fa7d3f24a1a2678a32a2b4a20bcb403680
Philip Brazeale <pbrazeale@gmail.com>
2025-03-22 10:03:46 -0500

    v0.39.12 Update Main

--- day_039_flight_deal_finder/main.py
+++ day_039_flight_deal_finder/main.py
@@ -1,16 +1,18 @@
+import time
 from data_manager import DataManager
+from flight_search import FlightSearch
 
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
 
-if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
-
-    flight_search = FlightSearch()
-    for row in sheet_data:
+for row in sheet_data:
+    if sheet_data[0]["iataCode"] == "":
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(f"sheet_data:\n {sheet_data}")
+        time.sleep(2)
 
-    data_manager.destination_data = sheet_data
-    data_manager.update_destination_codes()
+print(f"sheet_data:\n {sheet_data}")
+
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
