8f2c7b292f3ce7aa52b87b132282a6b3616dbb18
Philip Brazeale <pbrazeale@gmail.com>
2025-04-01 10:34:29 -0500

    0.45.1 Update Main soup

---
+++ day_045_web_scraping/main.py
@@ -0,0 +1,10 @@
+# Beautiful Soup Docs https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+
+# import lxml (sometimes needed if "html.parser" doesn't work)
+
+from bs4 import BeautifulSoup
+
+with open("website.html", "r") as webstie:
+    website_contents = webstie.read()
+
+soup = BeautifulSoup(website_contents, "html.parser")
